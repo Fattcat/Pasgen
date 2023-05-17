81a19ffbc0f1f113f3a9d4a7a48a284f5c8717bf
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-05-17 23:21:29 +0200

    Create Passgen.py

---
+++ Passgen.py
@@ -0,0 +1,21 @@
+from time import sleep
+import random
+
+lower_case = "abcdefghijklmnopqrstuvwxyz"
+UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+symbols = "/_<>[]!@#$%&()-:;.,?{}"
+numbs = "0123456789"
+
+def LenghtPassOSEM():
+  lenght = 8
+def LenghtPassDESAC():
+  lenght = 10
+def LenghtPassDVANASC():
+  lenght = 12
+def LenghtPassSESNAST():
+  lenght = 16
+
+lenght = 9
+Pridavac = lower_case + UPPER_CASE + symbols + numbs
+PSSWD = "".join(random.sample(Pridavac, lenght))
+print("Vase nove heslo je : ", PSWD)
